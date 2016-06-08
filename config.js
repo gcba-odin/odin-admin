d4c3958780948bee4793044c2a331dfda341fb38
Cristian Garcia <informacion@cristiangarcia.co>
2016-06-08 12:38:55 -0200

    adding link user in view datasets

--- config.js
+++ config.js
@@ -2,9 +2,10 @@
     var app = angular.module('config-odin',[]);
     app.run(run);
     function run($rootScope, $location, $cookieStore, $http) {
+    	$rootScope.url='http://137.135.84.77:3000';
         $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
             $rootScope.actualUrl=current.$$route.originalPath;
-            $rootScope.url='http://137.135.84.77:3000';
+    
         });
     }
 })(); 
