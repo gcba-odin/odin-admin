0dd00923d8491144ac56c6c3b849ecda174371d6
Crsitian Camilo Garcia <informacion@cristiangarcia.co>
2016-06-21 15:12:45 -0300

    Update fileController.js

--- js/controllers/fileController.js
+++ js/controllers/fileController.js
@@ -42,12 +42,23 @@ function FileListController($scope, $location, rest, $rootScope, Flash, Alertify
     modelService.loadAll($scope);
 }
 
-function FileViewController($scope, Flash, rest, $routeParams, $location, modelService) {
+function FileViewController($scope, Flash, rest, $routeParams, $location, modelService,$sce) {
     modelService.initService("File", "files", $scope);
+       $scope.getHtml = function(html) {
+            return $sce.trustAsHtml(html);
+        };
 
-
-    modelService.findOne($routeParams, $scope);
-
+        $scope.model = rest().findOne({
+            id: $routeParams.id,
+            type: $scope.type,
+            params:"include=tags"
+        },function (){
+            var tags=[];
+            for (var i = 0; i < $scope.model.tags.length; i++) {
+                tags.push('<span class="label label-primary">'+$scope.model.tags[i].name+'</span>'); 
+            };
+             $scope.model.tags=tags.join(" - ");
+        });
 
     $scope.edit = function(model) {
         modelService.edit($scope, model);
@@ -57,22 +68,6 @@ function FileViewController($scope, Flash, rest, $routeParams, $location, modelS
 function FileCreateController($scope, $sce, rest, model, Flash, $location, Upload, $rootScope, modelService) {
     modelService.initService("File", "files", $scope);
 
-
-
-    $scope.readURL = function(input) {
-
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function(e) {
-                console.log(e);
-                // $('#blah').attr('src', e.target.result);
-            }
-
-            //  reader.readAsDataURL(input.files[0]);
-        }
-    }
-
     $scope.clearUpload = function() {
         $scope.fileModel.name = "";
         $scope.fileModel.type = "";
@@ -80,6 +75,7 @@ function FileCreateController($scope, $sce, rest, model, Flash, $location, Uploa
 
     $scope.beforeChange = function($files) {
         $scope.fileModel.name = $files[0].name;
+        $scope.model.name=$scope.fileModel.name;
         var type = $files[0].name.split('.').pop();
         if (type == "doc" || type == "docx") {
             $scope.fileModel.type = 'fa-file-word-o';
@@ -103,7 +99,6 @@ function FileCreateController($scope, $sce, rest, model, Flash, $location, Uploa
     $scope.fileModel = []
     $scope.checkstep = function(step) {
         if (($scope.fileModel.name && step == 1) || ($scope.fileModel.name && step == 2) || step == 0) {
-
             if (step == 0) {
                 $scope.steps[0] = "active";
                 $scope.steps[1] = "undone";
@@ -139,28 +134,21 @@ function FileCreateController($scope, $sce, rest, model, Flash, $location, Uploa
         return $sce.trustAsHtml(html);
     };
 
-    $scope.addTag= function (name){
-        $rootScope.model.filetypes = rest().save({
-            type: "filetypes"
-        },{name:name},function (){
-            console.log("tag agregado");
-        });
-    }
-
     $scope.add = function(isValid) {
         $scope.uploadImageProgress = 10;
         var data = {
             'name': $scope.model.name,
-            'type': $scope.model.type,
             'status': $scope.model.status,
             'organization': $scope.model.organization,
             'dataset': $scope.model.dataset,
             'description': $scope.model.description,
             'notes': $scope.model.notes,
             'url': $scope.model.url,
-            'visibility': $scope.model.visibility,
-            ///'fileType':$scope.fileModel.type,
-            'uploadFile': $scope.model.uploadFile
+            'visible': $scope.model.visible,
+            'owner': $scope.model.owner,
+            'updateFrequency':$scope.model.updateFrequency,
+            'tags': $scope.model.tags.join(","),
+            'uploadFile': $scope.model.uploadFile,
         }
 
         Upload.upload({
@@ -178,45 +166,109 @@ function FileCreateController($scope, $sce, rest, model, Flash, $location, Uploa
     };
 
 
-    /*$scope.model.filetypes = rest().get({
-        type: "filetypes" ,params:"sort=name DESC"
-    });
-    $scope.model.statuses = rest().get({
-        type: "statuses" ,params:"sort=name DESC"
-    });
-  /*  $scope.model.organizations = rest().get({
-        type: "organizations" ,params:"sort=name DESC"
-    });
-    $scope.model.frequencies = rest().get({
-        type: "updatefrequencies" ,params:"sort=name DESC"
-    });
-    $scope.tagsmodel = rest().get({
-        type: "tags" ,params:"sort=name DESC"
-    });*/
 
 }
 
-function FileEditController($scope, Flash, rest, $routeParams, model, $location, modelService) {
+function FileEditController($rootScope,$scope, Flash, rest, $routeParams, model, $location, modelService,$sce,Upload) {
     modelService.initService("File", "files", $scope);
-
-
     $scope.model = new model();
+
+ $scope.model = new model();
+    $scope.steps = [];
+    $scope.steps[0] = "active";
+    $scope.steps[1] = "undone";
+    $scope.steps[2] = "undone";
+    $scope.stepactive = 0;
+
+    $scope.fileModel = []
+
+    $scope.checkstep = function(step) {
+        if (($scope.fileModel.name && step == 1) || ($scope.fileModel.name && step == 2) || step == 0) {
+            if (step == 0) {
+                $scope.steps[0] = "active";
+                $scope.steps[1] = "undone";
+                $scope.steps[2] = "undone";
+            } else if (step == 1) {
+                $scope.steps[0] = "done";
+                $scope.steps[1] = "active";
+                $scope.steps[2] = "undone";
+            } else {
+                $scope.steps[0] = "done";
+                $scope.steps[1] = "done";
+                $scope.steps[2] = "active";
+            }
+            console.log(step);
+            $scope.stepactive = step;
+        }
+    }
+    $scope.step = function(step) {
+        if (($scope.fileModel.name && step == 1) || ($scope.fileModel.name && step == 2) || step == 0) {
+            var step = $scope.steps[step];
+            if (step == "undone") {
+                return "undone";
+            } else if (step == "done") {
+                return "done";
+            } else {
+                return "active";
+
+            }
+        }
+
+    }
+    $scope.getHtml = function(html) {
+        return $sce.trustAsHtml(html);
+    };
+
+
     $scope.update = function(isValid) {
-        if (isValid) {
+  
+
+   $scope.uploadImageProgress = 10;
+        var data = {
+            'status': $scope.model.status,
+            'organization': $scope.model.organization,
+            'dataset': $scope.model.dataset,
+            'description': $scope.model.description,
+            'notes': $scope.model.notes,
+            'url': $scope.model.url,
+            'visible': $scope.model.visible,
+            'owner': $scope.model.owner,
+            'updateFrequency':$scope.model.updateFrequency,
+            'tags': $scope.model.tags.join(","),
+        }
+
+      if (isValid) {
             rest().update({
                 type: $scope.type,
                 id: $scope.model.id
-            }, $scope.model, function(resp) {
+            }, data, function(resp) {
                 var url = '/' + $scope.type;
                 $location.path(url);
             });
         }
+
+
     };
 
     $scope.load = function() {
         $scope.model = rest().findOne({
             id: $routeParams.id,
-            type: $scope.type
+            type: $scope.type,
+            params:"include=tags"
+        },function (){
+            $scope.fileModel.name=$scope.model.name
+            var type = $scope.fileModel.name.split('.').pop();
+            if (type == "doc" || type == "docx") {
+                $scope.fileModel.type = 'fa-file-word-o';
+            } else if (type == "xlsx" || type == "xls") {
+                $scope.fileModel.type = 'fa-file-excel-o';
+            } else if (type == "pdf") {
+                $scope.fileModel.type = 'fa-file-pdf-o';
+            } else if (type == "rar" || type == "zip") {
+                $scope.fileModel.type = 'fa-file-archive-o';
+            } else {
+                $scope.fileModel.type = 'fa-file-text-o';
+            }
         });
     };
 
