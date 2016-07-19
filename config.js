798b2a2b446d1ec98b520b28ea6cb0ede0118343
Ignacio Siri <ignaciosiri@Ignacios-MacBook-Pro.local>
2016-07-19 14:23:47 -0300

    fixing file upload.

--- config.js
+++ config.js
@@ -4,7 +4,9 @@
     function run($rootScope, $location, $cookieStore, $http) {
         $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
             $rootScope.actualUrl=current.$$route.originalPath;
-            $rootScope.url='http://104.208.247.131/api';
+           $rootScope.url='http://104.208.247.131/api';
+          //  $rootScope.url='http://localhost:3000';
+
         });
     }
 })();
