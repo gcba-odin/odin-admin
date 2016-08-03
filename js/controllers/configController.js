8cc7ddcc9050ccf8614e4e218170cbc9d27fe80d
arielleguizamon <ariel.leguizamon93@gmail.com>
2016-08-03 17:24:43 -0300

    Crud configs

---
+++ js/controllers/configController.js
@@ -0,0 +1,102 @@
+var app = angular.module('odin.configsControllers', []);
+
+app.factory('model', function($resource) {
+    return $resource();
+});
+
+function ConfigListController($scope, $location, rest, $rootScope, Flash, Alertify, modelService) {
+
+    modelService.initService("Config", "configs", $scope);
+
+    $scope.confirmDelete = function(item) {
+        modelService.confirmDelete(item);
+    }
+
+    $scope.deleteModel = function(model) {
+        modelService.delete($scope, model);
+    };
+
+    $scope.edit = function(model) {
+        modelService.edit($scope, model);
+    }
+
+    $scope.view = function(model) {
+        modelService.view($scope, model);
+    }
+
+    modelService.loadAll($scope);
+}
+
+function ConfigViewController($scope, Flash, rest, $routeParams, $location, modelService) {
+    modelService.initService("Config", "configs", $scope);
+
+    modelService.findOne($routeParams, $scope);
+
+    $scope.edit = function(model) {
+        var url = '/' + $scope.type + '/' + model.id + "/edit";
+        $location.path(url);
+    }
+}
+
+function ConfigCreateController($scope, rest, model, Flash, $location, modelService, Alertify, usSpinnerService) {
+
+    modelService.initService("Config", "configs", $scope);
+
+
+    $scope.model = new model();
+    $scope.add = function(isValid) {
+        usSpinnerService.spin('spinner');
+        if (isValid) {
+            rest().save({
+                type: $scope.type
+            }, $scope.model, function(resp) {
+                usSpinnerService.stop('spinner');
+                var url = '/' + $scope.type;
+                $location.path(url);
+            }, function(error) {
+                usSpinnerService.stop('spinner');
+                if (!!error.data.links.name[0]) {
+                    Alertify.alert('La configuracion que quiere guardar ya existe.');
+                } else {
+                    Alertify.alert('Hubo un error al crear la configuracion.');
+                }
+            });
+        }
+    };
+}
+
+function ConfigEditController($scope, Flash, rest, $routeParams, model, $location, modelService, Alertify, usSpinnerService) {
+    modelService.initService("Config", "configs", $scope);
+
+
+    $scope.model = new model();
+    $scope.update = function(isValid) {
+        usSpinnerService.spin('spinner');
+        if (isValid) {
+            rest().update({
+                type: $scope.type,
+                id: $scope.model.id
+            }, $scope.model, function(resp) {
+                usSpinnerService.stop('spinner');
+                var url = '/' + $scope.type;
+                $location.path(url);
+            }, function(error) {
+                usSpinnerService.stop('spinner');
+                if (!!error.data.links.name[0]) {
+                    Alertify.alert('La configuracion que quiere guardar ya existe.');
+                } else {
+                    Alertify.alert('Hubo un error al crear la configuracion.');
+                }
+            });
+        }
+    };
+
+    $scope.load = function() {
+        $scope.model = rest().findOne({
+            id: $routeParams.id,
+            type: $scope.type
+        });
+    };
+
+    $scope.load();
+}
