8f675f4e8dee743e3c97503cf7cde21b1f28f8bf
Maximiliano Angeletti <max.angletti@gmail.com>
2016-08-17 09:08:03 -0300

    config.js url

--- js/app.js
+++ js/app.js
@@ -331,7 +331,7 @@
     app.run(run);
 
     function run($rootScope, $location, $cookieStore, $http) {
-        $rootScope.url = 'http://40.121.80.86/api';
+        $rootScope.url = 'http://104.208.247.131/api';
         // keep user logged in after page refresh
 
     }
