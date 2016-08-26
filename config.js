d8e56cc6bd7054ddeb8b981f8706f6a60a8668a6
Maximiliano Angeletti <max.angletti@gmail.com>
2016-08-26 16:23:30 -0300

    connect stablished

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
         $rootScope.$on('$routeChangeSuccess', function (e, current, pre) {
             $rootScope.actualUrl=current.$$route.originalPath;
 //            $rootScope.url='http://localhost:3000';
-            $rootScope.url='http://40.76.56.16/api';
+            $rootScope.url='http://40.121.80.86/api';
 
         });
     }
