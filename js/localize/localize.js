2424b9eaf408ec88513e9585471d8a91e9b38bc3
Ignacio Siri <ignaciosiri@onion.local>
2016-09-26 17:12:32 -0300

    added user manual download

--- js/localize/localize.js
+++ js/localize/localize.js
@@ -361,7 +361,8 @@ app.config(['$translateProvider', function($translateProvider, $rootScope) {
     'CHARGE_OF_DATA': 'Carga de datos',
     'WEBSERVICE_URL': 'Dirección al servicio web',
     'WEBSERVICE_TYPE': 'Tipo de servicio web',
-    'SELECT_OPTION': 'Elije una opción'
+    'SELECT_OPTION': 'Elije una opción',
+    'USER_MANUAL': 'Manual de usuario'
 
 
 
